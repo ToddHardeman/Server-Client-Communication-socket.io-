cbcb33abcddfd1902a0651d7102d7f8aeea61116
Todd Hardeman <toddhardeman6@gmail.com>
2025-06-04 09:49:56 -0700

    got messages to emit from the server to the client

--- server/server.js
+++ server/server.js
@@ -17,13 +17,18 @@ app.get('/', (req, res) => {
 io.on('connection', (socket) => {
   console.log('a user connected');
   
-  socket.on('chat message', (msg) => {
-    console.log('message: ' + msg);
-  });
+    socket.on('chat message', (msg) => {
+        console.log('message: ' + msg);
+    });
 
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
-  });
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+
+
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+    });
 });
 
 server.listen(3000, () => {
